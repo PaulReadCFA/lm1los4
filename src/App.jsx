fab109c59dc38ea495b263f3fdc6c4114114efb0
PaulReadCFA <paul.read@cfainstitute.org>
2025-06-27 16:54:15 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,3 +8,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "#f0f0f0", padding: "2rem", fontSize: "1.5rem" }}>
+      React is working!
+    </div>
+  );
+}
+
+export default App;
