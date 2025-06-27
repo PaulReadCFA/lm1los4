7b01b00ee9a349b566676110320fa47a7a52e56d
PaulReadCFA <paul.read@cfainstitute.org>
2025-06-27 16:56:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-function App() {
+const App = () => {
   return (
     <div style={{ backgroundColor: "#f0f0f0", padding: "2rem", fontSize: "1.5rem" }}>
       React is working!
     </div>
   );
-}
+};
 
 export default App;
